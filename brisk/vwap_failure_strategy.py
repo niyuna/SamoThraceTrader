62c2161f8982b63e3a58cb13f4656dd8be5841a1
yuzx09 <yu@zhoux.me>
2025-08-31 17:54:05 +0900

    Update vwap_failure_strategy.py

--- brisk/vwap_failure_strategy.py
+++ brisk/vwap_failure_strategy.py
@@ -913,8 +913,8 @@ def main():
             
             exit_vol_ma5_ratio_threshold=4.5,
             force_exit_atr_factor=0.5, # temperarily disable this by setting a very huge value, we don't see this to be really useful
+            black_list=[],
 
-            # disable it for now
             gap_up_threshold=0.02,      # 2% gap up 
             failure_threshold_gap_up=30,        # Gap Up时的VWAP failure次数阈值
             entry_factor_gap_up=1.3,           # Entry ATR倍数
@@ -930,7 +930,6 @@ def main():
             max_daily_trades_gap_down=2,       # 单日最大交易次数
             max_exit_wait_time_gap_down=40,    # 最大平仓等待时间（分钟）
             max_vol_ma5_ratio_threshold_gap_down=3.0, # Gap Down时的成交量MA5阈值
-            black_list=[],
         )
 
         # 配置Mock Gateway的replay模式
