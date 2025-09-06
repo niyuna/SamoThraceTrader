4f9b3485d54941aa78f4c2cf0377b15fd287ec70
yuzx09 <yu@zhoux.me>
2025-09-06 16:31:20 +0900

    add weekday limitation to vwapfailure, also adjust parameters in yaml

--- brisk/vwap_failure_strategy.py
+++ brisk/vwap_failure_strategy.py
@@ -913,6 +913,7 @@ def main():
     strategy = VWAPFailureStrategy(use_mock_gateway=False, enable_delayed_entry=True)
     
     try:
+        weekday = datetime.now().weekday()
         # 设置策略参数
         strategy.set_strategy_params(
             market_cap_threshold=100_000_000_000,  # 1000亿日元
@@ -925,7 +926,7 @@ def main():
             force_exit_atr_factor=0.5, # temperarily disable this by setting a very huge value, we don't see this to be really useful
             black_list=[],
 
-            gap_up_threshold=0.5,      # 2% gap up 
+            gap_up_threshold=0.5 if weekday in [2, 3] else 0.02,      # 2% gap up 
             failure_threshold_gap_up=30,        # Gap Up时的VWAP failure次数阈值
             entry_factor_gap_up=1.3,           # Entry ATR倍数
             exit_factor_gap_up=1.7,            # Exit ATR倍数
